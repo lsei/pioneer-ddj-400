7747acdf9c09cfe10acb684a2b4866d7a9ba69fa
Lucas <lsei@users.noreply.github.com>
2020-12-14 15:43:30 +0000

    first commit

---
+++ pioneer/ddj.js
@@ -0,0 +1,116 @@
+import easymidi from "easymidi";
+import { left, HI_RES_CONTROLS } from "./midimap.js";
+
+class DDJ {
+    defaultOptions = {
+        padRows: 2,
+        padCols: 4,
+        intialPadMode: "HOT_CUE",
+        logOutgoingCommands: false,
+        logIncomingCommands: false, // TODO: implement
+
+        onFaderChange: (level) => {},
+        onLeftvolumeChange: (level) => {},
+        onRightvolumeChange: (level) => {},
+        onLeftfilterChange: (level) => {},
+        onRightfilterChange: (level) => {},
+    };
+
+    constructor(name = "DDJ-400", options = {}) {
+        this.options = {
+            ...this.defaultOptions,
+            ...options,
+        };
+
+        this.input = new easymidi.Input(name);
+        this.output = new easymidi.Output(name);
+        this.startListening();
+
+        this.state = {
+            padMode: this.options.intialPadMode,
+            controls: {
+                fader: new HighResValue(63, 0),
+                leftvolume: new HighResValue(63, 0),
+                rightvolume: new HighResValue(63, 0),
+                leftfilter: new HighResValue(63, 0),
+                rightfilter: new HighResValue(63, 0),
+            },
+        };
+    }
+
+    startListening() {
+        // this.input.on("noteon", function (msg) {
+        //     console.log(msg);
+        // });
+
+        let lastCC = {};
+
+        this.input.on("cc", (msg) => {
+            const key = `${msg.channel}_${msg.controller}`;
+            console.log(key);
+            const control = HI_RES_CONTROLS[key];
+            if (control) {
+                const majorValue = lastCC[control.major];
+                this.state.controls[control.name].set(majorValue, msg.value);
+                this.options[`on${capitalizeFirstLetter(control.name)}Change`](this.state.controls[control.name]);
+            }
+            lastCC[key] = msg.value;
+        });
+    }
+
+    playLeft(on = true) {
+        this._triggerButton(left.PLAY_PAUSE, on);
+    }
+
+    padModeHotCue() {
+        this._setPadMode("HOT_CUE");
+    }
+
+    pad(row, col, on = true) {
+        const key = `${this.state.padMode}_PAD_${row}_${col}`;
+        this._triggerButton(left[key], on);
+    }
+
+    // HOT_CUE, BEAT_LOOP, BEAT_JUMP, SAMPLER
+    _setPadMode(mode) {
+        this.state.padMode = mode;
+        this._triggerButton(left[mode], true);
+    }
+
+    _triggerButton([channel, note], on) {
+        this._send("noteon", {
+            channel,
+            note,
+            velocity: on ? 127 : 0,
+        });
+    }
+
+    _send(event, params) {
+        this.output.send(event, params);
+    }
+}
+
+export default DDJ;
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+class HighResValue {
+    constructor(major, minor) {
+        this.set(major, minor);
+    }
+
+    toFloat() {
+        return this.major + this.minor / 127;
+    }
+
+    toString() {
+        return (this.major + this.minor / 127).toString();
+    }
+
+    set(major, minor) {
+        this.major = major;
+        this.minor = minor;
+    }
+}
---
+++ pioneer/midimap.js
@@ -0,0 +1,66 @@
+export const channels = {
+    LEFT_MAIN: 0,
+    LEFT_PADS: 7,
+
+    RIGHT_MAIN: 1,
+    RIGHT_PADS: 9,
+};
+
+export const left = {
+    PLAY_PAUSE: [channels.LEFT_MAIN, 11],
+    CUE: [channels.LEFT_MAIN, 12],
+    SHIFT: [channels.LEFT_MAIN, 63],
+
+    // PAD MENU
+    HOT_CUE: [channels.LEFT_MAIN, 27],
+    BEAT_LOOP: [channels.LEFT_MAIN, 109],
+    BEAT_JUMP: [channels.LEFT_MAIN, 32],
+    SAMPLER: [channels.LEFT_MAIN, 34],
+
+    // PADS
+    HOT_CUE_PAD_0_0: [channels.LEFT_PADS, 0],
+    HOT_CUE_PAD_0_1: [channels.LEFT_PADS, 1],
+    HOT_CUE_PAD_0_2: [channels.LEFT_PADS, 2],
+    HOT_CUE_PAD_0_3: [channels.LEFT_PADS, 3],
+    HOT_CUE_PAD_1_0: [channels.LEFT_PADS, 4],
+    HOT_CUE_PAD_1_1: [channels.LEFT_PADS, 5],
+    HOT_CUE_PAD_1_2: [channels.LEFT_PADS, 6],
+    HOT_CUE_PAD_1_3: [channels.LEFT_PADS, 7],
+
+    SAMPLER_PAD_0_0: [channels.LEFT_PADS, 48],
+    SAMPLER_PAD_0_1: [channels.LEFT_PADS, 49],
+    SAMPLER_PAD_0_2: [channels.LEFT_PADS, 50],
+    SAMPLER_PAD_0_3: [channels.LEFT_PADS, 51],
+    SAMPLER_PAD_1_0: [channels.LEFT_PADS, 52],
+    SAMPLER_PAD_1_1: [channels.LEFT_PADS, 53],
+    SAMPLER_PAD_1_2: [channels.LEFT_PADS, 54],
+    SAMPLER_PAD_1_3: [channels.LEFT_PADS, 55],
+};
+
+export const HI_RES_CONTROLS = {
+    "6_63": {
+        name: "fader",
+        major: "6_31",
+        minor: "6_63",
+    },
+    "0_51": {
+        name: "leftvolume",
+        major: "0_19",
+        minor: "0_51",
+    },
+    "1_51": {
+        name: "rightvolume",
+        major: "1_19",
+        minor: "1_51",
+    },
+    "6_55": {
+        name: "leftfilter",
+        major: "6_23",
+        minor: "6_55",
+    },
+    "6_56": {
+        name: "rightfilter",
+        major: "6_24",
+        minor: "6_56",
+    },
+};
