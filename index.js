daa5407dd325ce6c13332aef46d5794ad6d44756
Lucas <lsei@users.noreply.github.com>
2020-12-14 17:04:14 +0000

    module v1

--- index.js
+++ index.js
@@ -2,37 +2,18 @@ import easymidi from "easymidi";
 import sleep from "./utils/sleep.js";
 import DDJ from "./pioneer/ddj.js";
 
-var inputs = easymidi.getInputs();
-var outputs = easymidi.getOutputs();
+// var inputs = easymidi.getInputs();
+// var outputs = easymidi.getOutputs();
 
-console.log("Inputs:", inputs);
-console.log("Outputs:", outputs);
+// console.log("Inputs:", inputs);
+// console.log("Outputs:", outputs);
 
-const ddj = new DDJ("DDJ-400", {
-    onFaderChange: (e) => console.log("fader" + e),
-    onLeftvolumeChange: (e) => console.log("left", e),
-    onRightvolumeChange: (e) => console.log("right", e),
-    onLeftfilterChange: (e) => console.log("leftfilter", e),
-    onRightfilterChange: (e) => console.log("rightfilter", e),
-});
+// const ddj = new DDJ("DDJ-400", {
+//     onFaderChange: (e) => console.log("fader" + e),
+//     onLeftvolumeChange: (e) => console.log("left", e),
+//     onRightvolumeChange: (e) => console.log("right", e),
+//     onLeftfilterChange: (e) => console.log("leftfilter", e),
+//     onRightfilterChange: (e) => console.log("rightfilter", e),
+// });
 
-ddj.padModeHotCue();
-ddj.playLeft();
-
-// ddj.playLeft(false);
-
-// while (true) {
-//     for (let row = 0; row < 2; row++) {
-//         for (let col = 0; col < 4; col++) {
-//             ddj.pad(row, col, true);
-//             await sleep(100);
-//         }
-//     }
-
-//     for (let row = 0; row < 2; row++) {
-//         for (let col = 0; col < 4; col++) {
-//             ddj.pad(row, col, false);
-//             await sleep(100);
-//         }
-//     }
-// }
+export { DDJ };
